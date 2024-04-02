cff47a2a6a44a1fbdf627818958d5e3b45b7a000
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-04-02 11:45:37 +0700

    Initial Commit

---
+++ beaure/beaureApp.swift
@@ -0,0 +1,17 @@
+//
+//  beaureApp.swift
+//  beaure
+//
+//  Created by Luthfi Misbachul Munir on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct beaureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
